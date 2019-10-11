76704cdeb5f2728783b6b02c9374de4fa687a132
sishenhei7 <982505136@qq.com>
2019-10-11 09:27:55 +0800

    fix: fix deploy bug

--- deploy-circleci.sh
+++ deploy-circleci.sh
@@ -4,6 +4,8 @@ cd docs
 cd .vuepress
 cd dist
 git init
+git config --global user.email "you@example.com"
+git config --global user.name "sishenhei7"
 git add .
 git commit -m 'deploy'
 git push -f git@github.com:sishenhei7/vue-blog.git master:gh-pages
