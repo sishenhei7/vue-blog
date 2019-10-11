efb2444fd72545236387a66aedc3774d7a3c2f6d
sishenhei7 <982505136@qq.com>
2019-10-11 11:06:20 +0800

    feat: add ci skip to commit msg

--- deploy-circleci.sh
+++ deploy-circleci.sh
@@ -7,5 +7,5 @@ git init
 git config --global user.email "you@example.com"
 git config --global user.name "sishenhei7"
 git add .
-git commit -m 'deploy'
+git commit -m 'deploy: deploy[ci skip]'
 git push -f git@github.com:sishenhei7/vue-blog.git master:gh-pages
